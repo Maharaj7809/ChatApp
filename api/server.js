e56c52e7602a88f757969056d2e669c333cd2971
Maharaj7809 <134138887+Maharaj7809@users.noreply.github.com>
2023-09-24 21:13:08 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'https://chatapp-ftl2.onrender.com',
+    origin: '*',
     methods: ['GET', 'POST']
   }
 })
